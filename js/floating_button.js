26d3a6654e7e35d4f7a9be14d3c96ee316f6a514
Paolo A. Esposito <48482788+HappyBarny@users.noreply.github.com>
2024-09-08 19:53:40 +0200

    Add files via upload
    Updated all the project to enhance mobile experience making it more similar to the desktop version. Added metatags for SEO and OG Tags

--- js/floating_button.js
+++ js/floating_button.js
@@ -7,6 +7,21 @@ function goToHomepage() {
     window.location.href = "/index.html";
 }
 
+
+window.onscroll = function() { toggleScrollButton() };
+
+window.onscroll = function() { toggleScrollButton() };
+
+function toggleScrollButton() {
+    const homeButton = document.getElementById('homeButton');
+    // Check if the user has scrolled 200px down the page
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        homeButton.classList.add('show'); // Add 'show' class to make the button visible
+    } else {
+        homeButton.classList.remove('show'); // Remove "show" class to hide the button
+    }
+}
+
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
